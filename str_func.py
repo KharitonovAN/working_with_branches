6d49e0282a14b7ff8f7a164b16e8f6bc8ce80234
HAN <han82@mail.ru>
2023-09-14 02:11:00 +0300

    add def + docstring

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@ def lineupper():
     """Функция для перевода текста заглавные буквы"""
     line = input()
     print(line.upper())
+
+
+def wordtitle():
+    """Как заказывали docstring"""
+    line = input()
+    print(line.title())
